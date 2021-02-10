cadee651a81ce3974e93fb4b5fa5f979afb02bbd
willhumble <will@humble.co.za>
2021-02-10 11:02:46 +0200

    Added Bridging Header

---
+++ example/ios/FlutterEscPos-Bridging-Header.h.h
@@ -0,0 +1,13 @@
+//
+//  FlutterEscPos-Bridging-Header.h.h
+//  Runner
+//
+//  Created by William Van Loggerenberg on 2021/02/10.
+//
+
+#ifndef FlutterEscPos_Bridging_Header_h_h
+#define FlutterEscPos_Bridging_Header_h_h
+
+#import "ePOS.h"
+
+#endif /* FlutterEscPos_Bridging_Header_h_h */
